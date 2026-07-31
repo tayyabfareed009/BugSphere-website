9e1f34b7ab2b2f4803d9024229c7b687cc1d83bc
Malik Tayyab Fareed <tayyabfareed009@gmail.com>
2026-07-31 12:12:50 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -74,7 +74,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 /* ===========================
    Middleware
